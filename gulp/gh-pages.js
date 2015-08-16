eb29d880a8cc97572175f7189e239ebb5019ab3b
yuriy.sergunin <enzo_amd@mail.ru>
2015-08-16 17:05:08 +0300

    Fix deploying to github-pages

--- gulp/gh-pages.js
+++ gulp/gh-pages.js
@@ -5,16 +5,16 @@ var htmlreplace = require('gulp-html-replace');
 
 var paths = require('./paths').paths;
 
-gulp.task('deploy', function() {
+gulp.task('gh-pages', function() {
     var srcIndex = gulp.src(paths.dist + '/index.html')
         .pipe(htmlreplace({
             baseTag: '<base href="/tir/">'
         }));
 
 
-    return streamqueue({ objectMode: true },
-        srcIndex,
-        gulp.src([paths.dist + '/**/*'])
+    return streamqueue({objectMode: true},
+        gulp.src([paths.dist + '/**/*']),
+        srcIndex
     )
         .pipe(ghPages({
             force: true

b5bdc4977ea69664b39d0a6c1f9d0ac87e1d7804
Yuriy Sergunin <yury.sergunin@sperasoft.com>
2015-09-14 19:41:37 +0300

    Update 2015-09-14T16:41:34.463Z

--- javascripts/app.js
+++ javascripts/app.js
@@ -618,6 +618,85 @@
 
 (function (module) {
 
+  module.directive('gunList', function () {
+    return {
+      templateUrl: 'modules/guns/tpl/gun-list.view.html',
+      scope: {},
+      controller: ["$scope", "Guns", function ($scope, Guns) {
+
+        // Pass fields to the $scope
+        _.assign($scope, {
+          guns: null
+        });
+
+        // Pass methods to the $scope
+        _.assign($scope, {
+
+        });
+
+
+        // Initialize
+
+        fetchGuns();
+
+
+        // Implementations
+
+        function fetchGuns() {
+          Guns.getGuns()
+            .then(function (guns) {
+              $scope.guns = guns;
+            });
+        }
+
+      }]
+    };
+  });
+
+})(angular.module('application'));
+
+/**
+ * Created by Yury.Sergunin on 28.08.2015.
+ */
+
+(function (module) {
+
+  module.directive('gunListItem', function () {
+    return {
+      templateUrl: 'modules/guns/tpl/gun-list-item.view.html',
+      scope: {
+        gun: '=gunListItem'
+      },
+      controller: ["$scope", "Guns", function ($scope, Guns) {
+
+        // Pass fields to the $scope
+        _.assign($scope, {
+
+        });
+
+        // Pass methods to the $scope
+        _.assign($scope, {
+
+        });
+
+
+        // Initialize
+
+
+        // Implementations
+
+      }]
+    };
+  });
+
+})(angular.module('application'));
+
+/**
+ * Created by Yury.Sergunin on 28.08.2015.
+ */
+
+(function (module) {
+
   module.directive('gunTile', function () {
     return {
       templateUrl: 'modules/guns/tpl/gun-tile.view.html',
